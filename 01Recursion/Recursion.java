c549c6c7bddacfa9aaa1506aed3bf676379b735a
Jonathan Huang <Jonathan Huang>
2017-02-06 22:44:34 -0500

    hw01

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -6,19 +6,18 @@ public class Recursion {
 
     public static double sqrt(double n) {
 	if (n < 0) throw new IllegalArgumentException();
-	return sqrtHelper(n / 2, 1, n);
+	return sqrtHelper(n, n);
     }
 
-    public static double sqrtHelper(double a, double b, double n) {
-	if (Math.abs(a * a - n) / n < 0.01) return a;
-	System.out.println(a + " " + a * a);
-	return sqrtHelper((a / b + b) / 2, a, n);
+    public static double sqrtHelper(double a, double n) {
+	if (Math.abs(a * a - n) / n < 0.000000000001) return a;
+	return sqrtHelper((n / a + a) / 2, n);
     }
 
     public static void main(String[] args) {
 	System.out.println(sqrt(100));
-	//System.out.println(sqrt(400));
-	//System.out.println(sqrt(90000));
+	System.out.println(sqrt(400));
+	System.out.println(sqrt(90000));
     }
     
 }
