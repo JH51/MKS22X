99224ae60848835af81245161c4e89ac53908b74
Jonathan Huang <jonhuang5@gmail.com>
2017-02-12 02:33:05 -0500

    hw02

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -29,7 +29,8 @@ public class QueenBoard {
 		addQueen(row, col);
 		return true && solveH(row + 1);
 	    }
-	    else removeQueen(row, col);
+	    //removeQueen(row);
+	    //solveH(row + 1);
 	}
 	return false;
     }
@@ -43,21 +44,26 @@ public class QueenBoard {
     }
 
     private void addQueen(int row, int col) {
-	board[row][col] = -4;
 	for (int i = 0; i < board.length; i += 1) {
 	    board[row][i] += 1;
 	    board[i][col] += 1;
 	}
 	int r = row;
 	int c = col;
-	while (r > 0 && c > 0) board[r--][c--] += 1;
+	while (r >= 0 && c >= 0) board[r--][c--] += 1;
+	r = row;
+	c = col;
+	while (r >= 0 && c < board.length) board[r--][c++] += 1;
 	r = row;
 	c = col;
 	while (r < board.length && c < board.length) board[r++][c++] += 1;
+	r = row;
+	c = col;
+	while (r < board.length && c >= 0) board[r++][c--] += 1;
+	board[row][col] = -1;
     }
 
     private void removeQueen(int row, int col) {
-	board[row][col] = -4;
 	for (int i = 0; i < board.length; i += 1) {
 	    board[row][i] -= 1;
 	    board[i][col] -= 1;
@@ -68,27 +74,51 @@ public class QueenBoard {
 	r = row;
 	c = col;
 	while (r < board.length && c < board.length) board[r++][c++] -= 1;
+	board[row][col] = 0;
+    }
+
+    private void removeQueen(int row) {
+	for (int col = 0; col < board.length; col += 1) {
+	    if (board[row][col] < 0) {
+		for (int i = 0; i < board.length; i += 1) {
+		    board[row][i] -= 1;
+		    board[i][col] -= 1;
+		}
+		int r = row;
+		int c = col;
+		while (r > 0 && c > 0) board[r--][c--] -= 1;
+		r = row;
+		c = col;
+		while (r < board.length && c < board.length) board[r++][c++] -= 1;
+		board[row][col] = 0;
+	    }
+	}
     }
 	
     public String toString() {
 	String boardString = "";
 	for (int row = 0; row < board.length; row += 1) {
-	    for (int col = 0; col < board.length; col += 1) {
-		//boardString += board[row][col] + " ";
-		
+	    for (int col = 0; col < board.length; col += 1) {	      
 		if (board[row][col] < 0) boardString += "Q ";
 		if (board[row][col] >= 0) boardString += "_ ";
-		//if (board[row][col] > 0) boardString += board[row][col] + " ";
-		
 	    }
+	    boardString += "\n";
+	}	
+	return boardString;
+    }
+
+    public String toStringDebug() {
+	String boardString = "";
+	for (int row = 0; row < board.length; row += 1) {
+	    for (int col = 0; col < board.length; col += 1) boardString += board[row][col] + " ";
 	    boardString += "\n";
 	}	
 	return boardString;
     }
     
     public static void main(String[] args) {
-	QueenBoard a = new QueenBoard(5);
-	System.out.println(a);
+	QueenBoard a = new QueenBoard(7);
+        //a.addQueen(2, 2);
 	System.out.println(a.solve());
 	System.out.println(a);
     }
