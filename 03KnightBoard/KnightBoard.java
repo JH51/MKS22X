38fdad1a97a28327acdeb28142657f09523da4a7
Jonathan Huang <jonhuang5@gmail.com>
2017-02-15 08:42:05 -0500

    hw03

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -3,13 +3,13 @@ public class KnightBoard {
     private int[][] board;
     
     public KnightBoard(int startingRows,int startingCols) {
-	board = new int[startingRows][startingCols];
+	board = new int[startingCols][startingRows];
     }
     
     public String toString() {
 	String boardString = "";
 	for (int row = 0; row < board.length; row += 1) {
-	    for (int col = 0; col < board.length; col += 1) {	      
+	    for (int col = 0; col < board[row].length; col += 1) {	      
 		if (board[row][col] < 10) boardString += " ";
 		boardString += board[row][col] + " ";
 	    }
@@ -22,6 +22,12 @@ public class KnightBoard {
     }
     
     private boolean solveH(int row ,int col, int level) {// level is the # of the knight
+	return true;
     }
 
+    public static void main(String[] args) {
+	KnightBoard a = new KnightBoard(7, 6);
+	System.out.println(a);
+    }
+    
 }
