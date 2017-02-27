fd5c91376f5ba243cedc5a82cf04a085f220b26e
Jonathan Huang <jonhuang5@gmail.com>
2017-02-27 08:37:09 -0500

    hw04

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,27 +1,27 @@
-public class Maze{
+public class Maze {
 
-    private char[][]maze;
+    private char[][] maze;
     private boolean animate;
 
-    public Maze(String filename, boolean ani){
+    public Maze(String filename, boolean ani) {
     }
     
-    public void setAnimate(boolean b){
+    public void setAnimate(boolean b) {
         animate = b;
     }
     
-    public boolean solve(){
+    public boolean solve() {
 	int startx=0,starty=0;
 	maze[startx][starty] = ' ';
 	return solve(startx,starty);
     }
     
-    private boolean solve(int x, int y){
-        if(animate){
+    private boolean solve(int x, int y) {
+        if (animate) {
             System.out.println(this);
             wait(20);
         }
-	return false; //so it compiles
+	return false;
     }
     
 }
