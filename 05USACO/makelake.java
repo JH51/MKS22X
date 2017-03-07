576bfb0789471581e84066c43f6bafa20f3c7269
Jonathan Huang <jonhuang5@gmail.com>
2017-03-06 19:47:40 -0500

    hw05

--- 05USACO/makelake.java
+++ 05USACO/makelake.java
@@ -14,11 +14,11 @@ public class makelake {
 	    lake = new int[3][3];
 	}
     }
-
+    
     public void stomp(int R_s, int C_s, int D_s) {
 	
     }
-
+    
     public static void main(String args[]) {
 	System.out.println(Integer.parseInt("4 6 22 2"));
     }
