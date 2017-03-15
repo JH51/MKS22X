1292a247e1419717b3a29cd5e7fd809defc833ba
Jonathan Huang <Jonathan Huang>
2017-03-14 21:28:06 -0400

    hw07

---
+++ 07/Select.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Select() {
+
+    public int quickSelect(int[] data, int k) {
+	return quickselect(data, 0, G.length - 1, k - 1);
+    }
+    
+    private int quickSelect(int[] data, int start, int end, int k) {
+	if (start <= end) {
+	    int pivot = part(data, start, end);
+	    if (pivot == k) return data[k];	    
+	    if (pivot > k) return quickSelect(data, start, end - 1, k);
+	    return quickSelect(data, pivot + 1, end, k);
+	}
+	return data[k];
+    }
+    
+    private int part(int[] data, int start, int last) {
+	int pivot = new Random().nextInt(end - start + 1) + start;
+	int temp = data[end];
+	data[end] = data[pivot];
+	data[pivot] = temp;
+	for (int i = start; i < end; i += 1) {
+	    if (data[i] > data[end]) {
+		temp = data[i];
+		data[i] = data[start];
+		data[start] = temp
+		start += 1;
+	    }
+	}
+	temp = data[start];
+	data[start] = data[end];
+	data[end] = temp;
+	return first;
+    }
+    
+}
