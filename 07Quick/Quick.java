d4c495d5bfcfa70e8d2aa0e664cf0448f6f0277e
Jonathan Huang <jonhuang5@gmail.com>
2017-03-16 14:35:31 -0400

    hw07

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -14,6 +14,18 @@ public class Quick {
 	}
 	return data[k];
     }
+
+    static int part(int[] data, int start, int end) {
+	int p = new Random().nextInt(end - start + 1) + start;
+	int pElement = data[p];
+	int[] temp = new int[data.length];
+	for (int i = start; i < end; i += 1) {
+	    if (data[i] < data[element) {
+		temp[start] = data[i];
+		start += 1;
+	    }
+	    if (data[i] > temp[end]) {
+		
     
     static int part(int[] data, int start, int end) {
 	int pivot = new Random().nextInt(end - start + 1) + start;
@@ -33,17 +45,5 @@ public class Quick {
 	data[end] = temp;
 	return start;
     }
-
-/*
-    public static void main(String[] args) {
-	int[] a = {2, 10, 15, 23, 0,  5};
-	System.out.println(select(a, 0));
-	System.out.println(select(a, 1));
-	System.out.println(select(a, 2));
-	System.out.println(select(a, 3));
-	System.out.println(select(a, 4));
-	System.out.println(select(a, 5));
-    }
-*/
     
 }
