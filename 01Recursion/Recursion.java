50f108bb1e29febdfc0b9a62caabf5aa8b6838d9
Jonathan Huang <Jonathan Huang>
2017-03-22 12:47:56 -0400

    Tests

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,17 +1,21 @@
 public class Recursion {
 
     public static String name() {
-	return "Huang,Jonathan";
+        return "Huang,Jonathan";
     }
 
     public static double sqrt(double n) {
-	if (n < 0) throw new IllegalArgumentException();
-	return sqrtHelper(n, n);
+        if (n <= 0) throw new IllegalArgumentException();
+        return sqrtHelper(n, n);
     }
 
     public static double sqrtHelper(double a, double n) {
-	if (Math.abs(a * a - n) / n < 0.000000000001) return a;
-	return sqrtHelper((n / a + a) / 2, n);
+        if (Math.abs(a * a - n) / n < 0.000000000001) return a;
+        return sqrtHelper((n / a + a) / 2, n);
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println(Test01.closeEnough(Math.sqrt(1024.0), sqrt(1024.0)));
+    }
+
 }
