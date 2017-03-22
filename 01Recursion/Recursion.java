51188903088942242e69f57daf5d2d29e50ed16f
Jonathan Huang <Jonathan Huang>
2017-03-22 12:55:31 -0400

    Edge cases

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,7 +5,8 @@ public class Recursion {
     }
 
     public static double sqrt(double n) {
-        if (n <= 0) throw new IllegalArgumentException();
+        if (n < 0.0) throw new IllegalArgumentException();
+        if (n == 0.0) return 0.0;
         return sqrtHelper(n, n);
     }
 
@@ -15,7 +16,7 @@ public class Recursion {
     }
 
     public static void main(String[] args) {
-        System.out.println(Test01.closeEnough(Math.sqrt(1024.0), sqrt(1024.0)));
+        System.out.println(sqrt(0.0));
     }
 
 }
