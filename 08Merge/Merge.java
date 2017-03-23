c1c1bc05f814907de44a7756366193d07d3f9cf9
Jonathan Huang <jonhuang5@gmail.com>
2017-03-23 14:42:58 -0400

    merge method

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,8 +1,46 @@
 public class Merge {
     
-    static int[] mergesort(int[] a) {
+    static void mergesort(int[] a) {
 	
     }
     
-    public void merge(int[] a, int[] b, int[] c) {
-	
+    static int[] merge(int[] a, int[] b, int[] dest) {
+	dest = new int[a.length + b.length];
+	int i = 0, j = 0, k = 0;
+	while (i < dest.length &&
+	       j < a.length &&
+	       k < b.length) {
+	    if (a[j] < b[k]) {
+		dest[i] = a[j];
+		j += 1;
+	    }
+	    else {
+		dest[i] = b[k];
+		k += 1;
+	    }
+	    i += 1;
+	}
+	if (j >= a.length) {
+	    while (i < dest.length && k < b.length) {
+		dest[i] = b[k];
+		i += 1;
+		k += 1;
+	    }
+	}
+	else if (k >= b.length) {
+	    while (i < dest.length && j < a.length) {
+		dest[i] = a[j];
+		i += 1;
+		j += 1;
+	    }
+	}
+	return dest;
+    }
+    
+    public static void main(String[] args) {
+	int[] a = {1, 3, 5, 6};
+	int[] b = {2, 4, 6, 7, 8};
+	for (int i : merge(a, b, new int[0])) System.out.print(i + " ");	
+    }
+
+}
