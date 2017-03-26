ef0a0514d5fd82f23ebfd2838975a748dacfe654
Jonathan Huang <Jonathan Huang>
2017-03-25 22:15:28 -0400

    MIN_VALUE to MAX_VALUE test case

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -6,31 +6,35 @@ public class Driver {
         String s = "";
         s += a[0] + ", ";
         s += a[1] + ", ";
-        s += ". . . ";
-        s += a[a.length - 2];
-        s += a[a.length - 1];
+        s += "... , ";
+        s += a[a.length - 2] + ", ";
+        s += a[a.length - 1] + ", ";
         return s;
     }
 
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+
         //int cases = Integer.parseInt(args[0]);
         //int size = Integer.parseInt(args[1]);
 
         //a. Randomized; minVal to maxVal
-        int score = 0;
-        int[][] a = new int[new Random().nextInt(1000)][new Random().nextInt(1000)];
-        for (int[] aa : a) {
-            for (int i = 0; i < aa.length; i += 1) {
-                aa[i] = new Random().nextInt(1000);
+        if (args[0].equals("1")) {
+            int score = 0;
+            int[][] a = new int[new Random().nextInt(1000)][new Random().nextInt(1000)];
+            for (int[] aa : a) {
+                for (int i = 0; i < aa.length; i += 1) {
+                    aa[i] = new Random().nextInt(Integer.MAX_VALUE) - new Random().nextInt(Integer.MAX_VALUE);
+                }
             }
+            for (int[] aa : a) {
+                Quick.quicksort(aa);
+                if (Quick.isSorted(aa)) score += 1;
+                else System.out.println("Error: " + toString(aa));
+            }
+            System.out.println(score + " / " + a.length);
         }
-        for (int[] aa : a) {
-            Quick.quicksort(aa);
-            if (Quick.isSorted(aa)) score += 1;
-            else System.out.println("Error: " + toString(aa));
-        }
-        System.out.println(score + " / " + a.length);
 
         //b. Randomized; a to b
 
@@ -42,6 +46,10 @@ public class Driver {
 
 
         //e. Reverse-sorted
+
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime + " ms");
 
     }
 
