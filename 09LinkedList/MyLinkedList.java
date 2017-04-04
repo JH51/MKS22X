675b3ab3c5b88d8083e627e2492fb1b69ea820c8
Jonathan Huang <Jonathan Huang>
2017-04-03 20:38:47 -0400

    add method appends to end

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -44,8 +44,8 @@ public class MyLinkedList {
     public MyLinkedList(int[] data) {
         start = new LNode();
         end = new LNode();
-        for (int i = data.length - 1; i >= 0; i -= 1) {
-            this.addToStart(data[i]);
+        for (int i = 0; i < data.length; i += 1) {
+            this.add(data[i]);
         }
         this.size = data.length;
     }
@@ -75,19 +75,23 @@ public class MyLinkedList {
         return tE;
     }
 
-    public void addToStart(int e) {
-        start = new LNode(e, this.start);
-        size += 1;
+    public boolean addToStart(int e) {
+        this.start = new LNode(e, this.start);
+        this.size += 1;
+        return true;
     }
 
-    public void add(int element) {
+    public boolean add(int element) {
         if (this.size == 0) start = new LNode(element);
         else {
             LNode tempNode = start;
             while (tempNode.next != null) tempNode = tempNode.next;
-            tempNode.next = new LNode(element);
+            LNode newNode = new LNode(element, null);
+            newNode.previous = tempNode;
+            tempNode.next = newNode;
         }
-        size +=  1;
+        this.size +=  1;
+        return true;
     }
 
     public int size() {
@@ -97,7 +101,7 @@ public class MyLinkedList {
     public String toString() {
         String string = "[";
         LNode tempNode = start;
-        while (tempNode.next != null) {
+        while (tempNode != null) {
             string += tempNode.value + ", ";
             tempNode = tempNode.next;
         }
@@ -111,7 +115,7 @@ public class MyLinkedList {
         int[] a = {0, 1, 2, 3, 4};
         MyLinkedList l = new MyLinkedList(a);
         System.out.println();
-        //l.add(5);
+        l.add(5);
         l.set(1, 10);
         System.out.println(l);
         for (int i : a) System.out.print(i + ", ");
