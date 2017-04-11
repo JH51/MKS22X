7b0cbfe1cd869dbf8b1397a0cd433d1b2786625d
Jonathan Huang <Jonathan Huang>
2017-04-11 15:20:05 -0400

    Start of something new

--- 10Deque/MyDeck.java
+++ 10Deque/MyDeck.java
@@ -5,19 +5,27 @@ public class MyDeque {
     LinkedList<String> list;
 
     void addFirst(String string) {
-
+        list.addFirst(string);
     }
 
     void addLast(String string) {
-
+        list.addLast(string);
     }
 
     String removeFirst() {
-
+        return list.remove(0);
     }
 
     String removeLast() {
+        return list.remove(list.size() - 1);
+    }
 
+    String getFirst() {
+        return list.get(0);
+    }
+
+    String getLast() {
+        return list.get(list.size() - 1);
     }
 
 }
