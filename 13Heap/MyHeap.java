419c7b976e1e31a27dbb5b25bc9b86487cb3a459
Jonathan Huang <jonhuang5@gmail.com>
2017-05-04 14:50:03 -0400

    Start

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class MyHeap {
+
+    private int minMax;
+    private ArrayList<String> a;
+
+    public MyHeap() {
+	a = new ArrayList<String>(1);
+	minMax = 1;
+    }
+    
+    MyHeap(boolean b) {
+	a = new ArrayList<String>(1);
+	minMax (b) ? -1 : 1;
+    }
+
+    void add(String s) {
+	a.add(s);
+	int i = i.size() - 1;
+	while (minMax * a.get(i).compareTo(a.get(i) / 2)) {
+	    i = pushUp(i);
+	}
+    }
+    
+    String remove() {
+
+    }
+
+    String peek() {
+	return a.get(1);
+    }
+	
+    private int pushUp(int i) {
+	String temp = a.get(i);
+	a.set(i, a.get(i / 2));
+	a.set(i / 2, temp);
+	return i / 2;
+    }
+
+    private pushDown() {
+
+    }
+
+	}
