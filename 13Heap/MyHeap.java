bf4f3b6ef994902cbd8891510f1155e6c66a74e4
Jonathan Huang <Jonathan Huang>
2017-05-21 20:39:46 -0400

    Fixed MyHeap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -24,14 +24,16 @@ public class MyHeap {
     }
 
     String remove() {
-        String temp = a.get(0);
-        a.set(0, a.get(a.size() - 1));
-        a.remove(a.size() - 1);
-        int i = 0;
-        while (i * 2 + 1 < a.size() &&
-                !isValid())
-            i = pushDown(i);
-        return temp;
+        String s = a.get(0);
+        if (a.size() == 1)
+            return a.remove(0);
+        else {
+            a.set(0, a.remove(a.size() - 1));
+            int i = 0;
+            while (i * 2 + 1 < a.size() && minMax * a.get(i).compareTo(a.get(i * 2 + 1)) <= 0)
+                i = pushDown(i);
+            return s;
+        }
     }
 
     String peek() {
@@ -46,20 +48,13 @@ public class MyHeap {
     }
 
     private int pushDown(int i) {
-        String t = a.get(i);
-        int j = i * 2 + 1;
-        if (j + 1 < a.size())
+        String s = a.get(i);
+        int j = 1;
+        if (i * 2 + 2 < a.size())
             j = (minMax * a.get(i * 2 + 1).compareTo(a.get(i * 2 + 2)) >= 0) ? 1 : 2;
         a.set(i, a.get(i * 2 + j));
-        a.set(i * 2 + j, t);
+        a.set(i * 2 + j, s);
         return i * 2 + j;
-    }
-
-    public boolean isValid() {
-        boolean b = true;
-        for (int i = 0; i < a.size() && b; i += 1)
-            b = minMax * a.get(i).compareTo(a.get(i / 2)) <= 0;
-        return b;
     }
 
     public String toString() {
@@ -68,5 +63,5 @@ public class MyHeap {
             s += a.get(i) + ", ";
         return s.substring(0, s.length() - 2) + "]";
     }
-
+    
 }
